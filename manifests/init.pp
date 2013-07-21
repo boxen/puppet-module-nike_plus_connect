ac1d50b4126d581c478a8f7024f11e25f21175ec
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-07-21 22:15:16 +0200

    Initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Installs Nike+ Connect
+#
+# Usage:
+#
+#   include nike_plus_connect
+#
+class nike_plus_connect {
+  package { 'NikePlusConnect':
+    provider => 'pkgdmg',
+    source   => 'http://nikeplus.nike.com/nikeplus/nikeconnect/installers/macosx/Nike+Connect_new.dmg'
+  }
 }
